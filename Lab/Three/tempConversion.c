defe921c3517b5007100ad1aaf3cdf77313b3e0a
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 21:50:35 +0530

    Create tempConversion.c

---
+++ Lab/Three/tempConversion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float C,F;
+	
+	printf("Enter temperature in degree Fahrenheit\n");
+	scanf("%f",&F);
+
+	C=(F-32)*5/9;
+	printf("%f degree Fahrenheit = %f degree Celcius\n", F,C);
+
+	printf("\nEnter temperature in degree Celcius\n");
+	scanf("%f",&C);
+
+	F=C*9/5+32;
+	printf("%f degree Celcius = %f degree Fahrenheit\n", C,F);
+
+	return 0;
+}
