1bc9953ded718fa00d69a106a9dc2ea39f572b37
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 21:01:01 +0530

    Create swap1.c

---
+++ Lab/Three/swap1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,tmp;
+
+	printf("Enter two numbers\n");
+
+	printf("A = ");
+	scanf("%d",&a);
+
+    printf("B = ");
+    scanf("%d",&b);
+
+    tmp=a;
+    a=b;
+    b=tmp;
+
+    printf("\nAfter swapping,\n");
+    printf("A = %d\n", a);
+    printf("B = %d\n", b);
+
+	return 0;
+}
