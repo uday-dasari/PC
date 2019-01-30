a25bcd1400f8d3f0d71a70b748659f9ab6704d48
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 20:51:43 +0530

    Create SumDig.c

---
+++ Lab/Three/SumDig.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num,sum=0;
+
+	printf("Enter a four digit number\n");
+	scanf("%d",&num);
+
+	sum+=num%10;
+	num/=10;
+
+	sum+=num%10;
+	num/=10;
+
+	sum+=num%10;
+	num/=10;
+
+	sum+=num%10;
+
+	printf("Sum of digits = %d\n",sum);
+	return 0;
+}
