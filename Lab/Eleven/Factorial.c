78abaad198c53c8d8b925a86de4938aadf001830
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 20:08:24 +0530

    Create Factorial.c

---
+++ Lab/Eleven/Factorial.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int Fact(int N) //Function to compute factorial. 
+{
+	int factorial=1;
+
+	for(int i=1;i<=N;i++)
+		factorial*=i;
+
+	return factorial;
+}
+int main()
+{
+	int N;
+
+	printf("Enter a number\n");
+	scanf("%d",&N);
+
+	if(N<0)
+		printf("Factorial is not defined for negative numbers\n");
+	else
+		printf("%d! = %d\n",N,Fact(N));
+
+	return 0;
+}
