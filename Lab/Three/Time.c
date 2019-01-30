349d5234578f45cd999e04a28ee9f41a22a59d0e
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 20:47:50 +0530

    Create Time.c

---
+++ Lab/Three/Time.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int s,h,m;
+	h=m=0;
+
+	printf("Enter Time in seconds\n");
+	scanf("%d",&s);
+
+	h=s/3600;
+	s%=3600;
+
+	m=s/60;
+	s%=60;
+
+	printf("\nHours = %d\n", h);
+	printf("Minutes = %d\n", m);
+	printf("Seconds = %d\n", s);
+
+	return 0;
+}
