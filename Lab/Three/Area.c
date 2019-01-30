d16bc6a1b4773747f26763091890f0b18ce636ae
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 21:04:39 +0530

    Create Area.c

---
+++ Lab/Three/Area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float r,area;
+
+	printf("Enter radius of circle\n");
+	scanf("%f",&r);
+
+	area=M_PI*pow(r,2);
+	printf("Area of circle = %f\n", area);
+	return 0;
+}
