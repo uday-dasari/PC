9480efbd638a865a7a4bde15831ea9120527e469
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-30 21:17:10 +0530

    Create distConversion.c

---
+++ Lab/Three/distConversion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float mm,cm,inch,feet;
+	cm=inch=feet=0;
+
+	printf("Enter distance in mm\n");
+	scanf("%f",&mm);
+
+	cm=mm/10;
+	inch=cm/2.5;
+	feet=inch/12;
+
+	printf("\n%f millimetres = \n\n", mm);
+	printf("%f centimetres\n", cm);
+	printf("%f inches\n", inch);
+	printf("%f feet\n", feet);
+
+	return 0;
+}
