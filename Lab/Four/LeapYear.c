2d804d0a2040bc6ee629ed2d09af6c825f21a5c9
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-31 18:07:04 +0530

    Create LeapYear.c

---
+++ Lab/Four/LeapYear.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int N;
+
+	printf("Enter a year\n");
+	scanf("%d",&N);
+
+	if(N%4==0)
+	{
+		if(N%100==0)
+		{
+			if(N%400==0)
+				printf("Leap Year\n");
+			else
+				printf("Not Leap Year\n");
+		}
+		else
+			printf("Leap Year\n");
+	}
+	else
+		printf("Not Leap Year\n");
+
+	return 0;
+}
