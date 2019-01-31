1024f027ef0e179a890cb42b8f138571b2eae15d
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-31 18:10:16 +0530

    Create multiple.c

---
+++ Lab/Four/multiple.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int N,M;
+
+	printf("Enter two numbers\n");
+	scanf("%d%d",&N,&M);
+
+	if(N==0||M==0||N%M==0||M%N==0)
+		printf("Yes\n");
+	else
+		printf("No\n");
+
+	return 0;
+}
