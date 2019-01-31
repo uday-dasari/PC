7621f16bd8c32a09469a8f6e4eb00a5d3940f43a
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-31 18:23:57 +0530

    Create Area.c

---
+++ Lab/Four/Area.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float a,b,c,S,Area;
+
+	printf("Enter three sides\n");
+	scanf("%f%f%f",&a,&b,&c);
+
+	if(!(a+b>c&&b+c>a&&c+a>b))
+		printf("Triangle not possible\n");
+	else
+	{
+
+		S=(a+b+c)/2;
+		Area=sqrt(S*(S-a)*(S-b)*(S-c));
+
+		printf("Area = %f\n",Area);
+	}
+
+	return 0;
+}
