5ad76da2a827f04928b616f06b4ac186aad4c4de
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-01-31 18:19:19 +0530

    Create Character.c

---
+++ Lab/Four/Character.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	char c;
+
+	printf("Enter a character\n");
+	scanf("%c",&c);
+
+	if(c>=48&&c<=57)
+		printf("Digit\n");
+	else
+	{
+		if(c>90)
+			c-=32;
+
+		if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+			printf("Vowel\n");
+		else
+			printf("Consonant\n");
+	}
+
+	return 0;
+}
