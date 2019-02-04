f2eb553ea219a0439ec56543dbc444de5a8723ba
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-04 10:15:06 +0530

    Create Grade.c

---
+++ Lab/Four/Grade.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	char grade;
+	int score;
+
+	printf("Enter your score\n");
+	scanf("%d",&score);
+
+	if(score>=9)
+		grade='A';
+	else if(score>=7)
+		grade='B';
+	else if(score>=5)
+		grade='C';
+	else if(score>=3)
+		grade='D';
+	else
+		grade='F';
+
+	printf("Your grade is: %c\n", grade);
+
+	return 0;
+}
