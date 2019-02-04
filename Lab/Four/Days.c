1166ead6334a075c4379723b47ecd86ed0660899
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-04 10:10:30 +0530

    Create Days.c

---
+++ Lab/Four/Days.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int m,d;
+
+	printf("Enter the month number\n");
+	scanf("%d",&m);
+
+	if(m==4||m==6||m==9||m==11)
+		d=30;
+	else if(m==2)
+		d=28;
+	else
+		d=31;
+
+	printf("Number of days = %d\n",d);
+
+	return 0;
+}
