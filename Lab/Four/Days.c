576041824a7f07e424b93cd1388e9f70427e588d
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-04 10:05:40 +0530

    Create Days.c

---
+++ Lab/Four/Days.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int m,d=0,y,i;
+
+	int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+	printf("Enter year\n");
+	scanf("%d",&y);
+
+	days[1]=(y%4==0)?((y%100==0)?((y%400==0)?29:28):29):28;
+
+
+	printf("Enter month number\n");
+	scanf("%d",&m);
+
+	for(i=0;i<m;i++)
+		d+=days[i];
+
+	printf("Total number of days = %d\n", d);
+
+	return 0;
+}
