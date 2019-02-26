d47ea85dfb87cb0131519f63947443b93f67266a
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 20:53:56 +0530

    Create GCD.c

---
+++ Lab/Eleven/GCD.c
@@ -0,0 +1,28 @@
+//created by akashbhalotia
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+	if(a==0)
+		return b;
+	return gcd(b%a,a);
+}
+
+int main()
+{
+	int g,a,b;
+
+	printf("Enter two numbers\n");
+	scanf("%d%d",&a,&b);
+
+	if(a<=0||b<=0)
+	{
+		printf("Invalid Input\n");
+		return 0;
+	}
+
+	g=gcd(a,b);
+	printf("GCD = %d\n",g);
+
+	return 0;
+}
