8047556777adef824cfb3f715ca596a7f5cae87b
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 18:30:06 +0530

    Create palin.c

---
+++ Lab/Seven/palin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int N,copy,rev=0;
+
+	printf("Enter a number\n");
+	scanf("%d",&N);
+
+	copy=N;
+
+	while(copy>0)
+	{
+		rev=rev*10+copy%10;
+		copy/=10;
+	}
+
+	if(rev==N)
+		printf("Palindrome\n");
+	else
+		printf("Not a palindrome\n");
+	
+	return 0;
+}
