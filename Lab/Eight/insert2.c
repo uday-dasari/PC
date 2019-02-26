a7f489ff1d381f0993c0b1af174f1e6575a5666f
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 13:52:51 +0530

    Create insert2.c

---
+++ Lab/Eight/insert2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main()
+{
+	int N,i,num,tmp,pos;
+
+	printf("Enter number of elements in the array\n");
+	scanf("%d",&N);
+
+	int a[N],b[N+1];
+
+	for(i=0;i<N;i++)
+	{		
+		printf("Enter a number\n");
+		scanf("%d",&a[i]);
+	}
+
+	printf("\nOriginal Array:\n");
+	for(i=0;i<N;i++)
+		printf("%d,",a[i]);
+
+	printf("\n");
+	printf("Enter the number to be inserted\n");
+	scanf("%d",&num);
+
+	pos=N;
+	
+	for(i=0;i<N;i++)
+	{
+		if(num<a[i])
+		{
+			pos=i;
+			break;
+		}
+	}
+
+	tmp=0;
+	
+	for(i=0;i<N+1;i++)
+	{
+		if(i==pos)
+		{
+			b[i]=num;
+			tmp--;
+		}
+		else
+			b[i]=a[tmp];
+
+		tmp++;
+	}
+
+	printf("\nNew Array:\n");
+	for(i=0;i<N+1;i++)
+		printf("%d,",b[i]);
+
+	printf("\n");
+	
+	return 0;
+}
