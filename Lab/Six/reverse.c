b6aac10360c937c5a9b0ef689f6bbf7907c39541
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 13:07:09 +0530

    Create reverse.c

---
+++ Lab/Six/reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n,rev=0;
+
+	printf("Enter a number\n");
+	scanf("%d",&n);
+
+	while(n>0)
+	{
+		rev=rev*10+n%10;
+		n/=10;
+	}
+	printf("Reverse = %d\n",rev);
+
+	return 0;
+}
