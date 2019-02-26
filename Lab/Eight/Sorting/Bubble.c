5d3ba5de445fa191e3f81c61a4e97e566e3e9c07
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 13:23:31 +0530

    Create Bubble.c

---
+++ Lab/Eight/Sorting/Bubble.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+	int N,i,j,tmp;
+
+	printf("Enter number of elements in the array\n");
+	scanf("%d",&N);
+
+	int a[N];
+
+	for(i=0;i<N;i++)
+	{		
+		printf("Enter a number\n");
+		scanf("%d",&a[i]);
+	}
+
+	printf("\nOriginal Arrays:\n");
+	for(i=0;i<N;i++)
+		printf("%d,",a[i]);
+
+	printf("\n");
+
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				tmp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=tmp;
+			}
+		}
+	}
+
+	printf("\nSorted Array:\n");
+	for(i=0;i<N;i++)
+		printf("%d,",a[i]);
+
+	printf("\n");
+	
+	return 0;
+}
