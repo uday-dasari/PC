0e6a41243e9deb3fcce731ed7c483f176698d37d
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 20:40:00 +0530

    Update Roots.c

--- Lab/Four/Roots.c
+++ Lab/Four/Roots.c
@@ -1,24 +1,41 @@
-#include<stdio.h>
-#include<math.h>
+//code taken from https://www.programiz.com/c-programming/examples/quadratic-roots
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	float a,b,c,D,r1,r2;
+    double a, b, c, discriminant, root1, root2, realPart, imaginaryPart;
 
-	printf("Enter coefficients\n");
-	scanf("%f%f%f",&a,&b,&c);
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf",&a, &b, &c);
 
-	D=pow(b,2)-4*a*c;
+    discriminant = b*b-4*a*c;
 
-	if(D<0)
-		printf("The roots are imaginary\n");
-	else
-	{
-		r1=(-b+sqrt(D))/(2*a);
-		r2=(-b-sqrt(D))/(2*a);
+    // condition for real and different roots
+    if (discriminant > 0)
+    {
+    // sqrt() function returns square root
+        root1 = (-b+sqrt(discriminant))/(2*a);
+        root2 = (-b-sqrt(discriminant))/(2*a);
 
-		printf("The roots are:\n");
-		printf("%f, %f\n",r1,r2);
-	}
-	return 0;
-}
+        printf("root1 = %.2lf and root2 = %.2lf",root1 , root2);
+    }
+
+    //condition for real and equal roots
+    else if (discriminant == 0)
+    {
+        root1 = root2 = -b/(2*a);
+
+        printf("root1 = root2 = %.2lf;", root1);
+    }
+
+    // if roots are not real 
+    else
+    {
+        realPart = -b/(2*a);
+        imaginaryPart = sqrt(-discriminant)/(2*a);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imaginaryPart, realPart, imaginaryPart);
+    }
+
+    return 0;
+}   
