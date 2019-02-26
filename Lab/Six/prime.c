1330e818a36c52a95d97e85645783c172376972c
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 12:01:05 +0530

    Create prime.c

---
+++ Lab/Six/prime.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+
+{		int m,n,c,i,j;
+
+	
+
+	printf("Enter the range\n");
+
+	scanf("%d%d",&m,&n);
+
+	m=(m<1)?1:m;
+
+	
+
+	printf("\nPrime numbers:\n");
+
+	for(i=m+1;i<n;i++)
+
+	{
+
+		c=0;
+
+		for(j=1;j<=i;j++)
+
+		{
+
+			if(i%j==0)
+
+				c++;
+
+		}
+
+		if(c==2)
+
+		printf("%d,",i);
+
+	}
+
+	
+
+	printf("\n");
+
+	return 0;
+
+}
