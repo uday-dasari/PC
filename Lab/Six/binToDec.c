0f89d2ca1aeda8f5f132995641b157018777e616
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 13:04:47 +0530

    Create binToDec.c

---
+++ Lab/Six/binToDec.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	long bin,tmp=1,dec=0;
+
+	printf("Enter binary number\n");
+	scanf("%ld",&bin);
+
+	while(bin>0)
+	{
+		dec+=(bin%2)*tmp;
+		bin/=10;
+		tmp*=2;
+	}
+
+	printf("Decimal equivalent = %ld\n",dec);
+
+	return 0;
+}
