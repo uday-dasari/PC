fe915e6f4a89025b22ece462943d49c02ed33097
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 19:45:57 +0530

    Create sinExpansion.c

---
+++ Lab/Six/sinExpansion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int N,i;
+	long double sum,X,tmp;
+
+	printf("Enter the value of angle in degrees and the number of terms\n");
+	scanf("%Lf%d",&X,&N);
+
+	X=X*M_PI/180;
+	tmp=sum=X;
+
+	for(i=2;i<=N;i++)
+	{
+		tmp=(tmp*(-1)*pow(X,2))/((i*2-1)*(i*2-2));
+		sum+=tmp;
+	}
+
+	printf("sin(x) approximates to : %Lf\n",sum);
+
+	return 0;
+}
