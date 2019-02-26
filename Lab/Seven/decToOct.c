9c6054ce9fc1e7eed195a51c322bf898595669f0
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 18:56:18 +0530

    Create decToOct.c

---
+++ Lab/Seven/decToOct.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int dec,oct=0,tmp=1;
+
+	printf("Enter Decimal number\n");
+	scanf("%d",&dec);
+
+	while(dec>0)
+	{
+		oct+=(dec%8)*tmp;
+		tmp*=10;
+		dec/=8;
+	}
+	
+	printf("Octal = %d\n",oct);
+	
+	return 0;
+}
