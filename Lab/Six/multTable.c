64070153ddaca161c83854fa7e525ffc6c263c07
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 19:50:29 +0530

    Create multTable.c

---
+++ Lab/Six/multTable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int N,K,i,j;
+
+	printf("Enter N and K\n");
+	scanf("%d%d",&N,&K);
+
+	for(i=1;i<=N;i++)
+	{
+		for(j=1;j<=K;j++)
+		{
+			printf("%d\t",(i*j));
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
