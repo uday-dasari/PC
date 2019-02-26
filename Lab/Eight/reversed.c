34c3d7812d5b6166cfdbc64b5cf2a79964f08ea2
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 13:35:47 +0530

    Create reversed.c

---
+++ Lab/Eight/reversed.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+	int N,i,j,tmp;
+
+	printf("Enter number of elements in the array\n");
+	scanf("%d",&N);
+
+	int a[N];
+
+	for(i=0;i<N;i++)
+	{		
+		printf("Enter a number\n");
+		scanf("%d",&a[i]);
+	}
+
+	printf("\nOriginal Array:\n");
+	for(i=0;i<N;i++)
+		printf("%d,",a[i]);
+
+	printf("\n");
+
+	for(i=0;i<N/2;i++)
+	{
+		tmp=a[i];
+		a[i]=a[N-i-1];
+		a[N-i-1]=tmp;
+	}
+
+	printf("\nReversed Array:\n");
+	for(i=0;i<N;i++)
+		printf("%d,",a[i]);
+
+	printf("\n");
+	
+	return 0;
+}
