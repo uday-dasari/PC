d1e5e9075f33994bcea0980ca92aad40d184bafc
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-02-26 12:55:40 +0530

    Update days.c

--- Lab/Five/days.c
+++ Lab/Five/days.c
@@ -1,59 +1,40 @@
 #include <stdio.h>
 
-int main()
-
+int main() 
 {
 
-	int days=0,m,y;	
+    int days = 0, m, y;
 
-	printf("Enter month and year\n");
+    printf("Enter month and year\n");
+    scanf("%d%d", &m, &y);
 
-	scanf("%d%d",&m,&y);
+    if (m < 1 || m > 12 || y < 0)
+    {
+        printf("Invalid Input\n");
+        return 0;
+    }
 
-	
+    switch (m) 
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            days = 31;
+            break;
+            
+        case 2:
+            days = (y % 4 == 0) ? ((y % 100 == 0) ? ((y % 400 == 0) ? 29 : 28) : 29) : 28;
+            break;
 
-	if(m<1||m>12||y<0)
+        default:
+            days = 30;
+    }
+    
+    printf("Days = %d\n", days);
 
-	{
-
-		printf("Invalid Input\n");
-
-		return 0;
-
-	}
-
-	switch(m)
-
-	{
-
-		case 1:
-
-		case 3:
-
-		case 5:
-
-		case 7:
-
-		case 8:
-
-		case 10:
-
-		case 12: days=31;
-
-			break;
-
-		case 2: days=(y%4==0)?((y%100==0)?((y%400==0)?29:28):29):28;
-
-		break;
-
-		default: days=30;
-
-	}
-
-	
-
-	printf("Days = %d\n",days);
-
-	return 0;
-
+    return 0;
 }
