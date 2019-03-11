6447fea66a4ad1ef7cb7a9ba4c577e1fb0eda4e9
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-03-11 22:21:00 +0530

    Create Max.c

---
+++ Lab/Eleven/Max.c
@@ -0,0 +1,36 @@
+//created by akashbhalotia
+#include <stdio.h>
+int max(int a, int b)
+{
+	return (a>b)?a:b;
+}
+int Max(int a[], int N)
+{
+	int i,ans=a[0];
+
+	for(i=1;i<N;i++)
+	{
+		ans=max(ans,a[i]);
+	}
+
+	return ans;
+}
+int main()
+{
+	int i,N;
+
+	printf("Enter size of array\n");
+	scanf("%d",&N);
+
+	int a[N];
+
+	for(i=0;i<N;i++)
+	{
+		printf("Enter a number\n");
+		scanf("%d",&a[i]);
+	}
+
+	printf("Max value = %d\n",Max(a,N));
+
+	return 0;
+}
