37dafd75858da06cc09e846ec36f070aeaf17205
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-03-11 22:13:59 +0530

    Create prime.c

---
+++ Lab/Eleven/prime.c
@@ -0,0 +1,34 @@
+//created by akashbhalotia
+#include <stdio.h>
+int isPrime(int a)
+{
+	int i,c=0;
+	for(i=1;i<=a;i++)
+	{
+		if(a%i==0)
+			c++;
+	}
+	
+	if(c==2)
+		return 1;
+
+	return 0;
+}
+int main()
+{
+	int i,N,count=0;
+
+	printf("Enter the value of N\n");
+	scanf("%d",&N);
+
+	for(i=1;count<N;i++)
+	{
+		if(isPrime(i))
+		{
+			count++;
+			printf("%d,",i);
+		}
+	}
+
+	return 0;
+}
